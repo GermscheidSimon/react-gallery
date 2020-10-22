83fbe3ca4cef044ad89b5f6b4d2b993d8912f26f
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2020-10-22 17:16:01 -0500

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Gallery of my life</h1>
+        </header>
+        <br/>
+        <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg"/>
+      </div>
+    );
+  }
+}
+
+export default App;
