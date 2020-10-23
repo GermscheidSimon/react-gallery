4ab7c4ab99ccaa3103509e68d5893f7d88b60972
Khaildoran <sgermscheid@gmail.com>
2020-10-23 09:03:45 -0500

    created initial strucutre of compontents

---
+++ src/components/GalelryItem/GalleryItem.js
@@ -0,0 +1,16 @@
+import react, { Component } from 'react'
+
+
+class GalleryItem extends Component {
+
+  
+
+    render(){
+        return (
+            <div id="galItemFrame">
+                
+            </div>
+        )
+    }
+
+}
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,20 @@
+const { Component } = require("react")
+
+import react, {Component} from 'react'
+
+class GalleryList extends Component {
+
+    state = {
+        data: [
+            { /*   need a caption, img URL, and number of likes for each component  */ }
+        ]
+    }
+
+    render(){
+        return (
+            <div>
+                {/* in here need to create a gallery item for each that i have data for passed from app js */}
+            </div>
+        )
+    }
+}
