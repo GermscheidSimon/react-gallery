b3e1279264043d9c0f1f15556607e7f5fef5a6d3
Khaildoran <sgermscheid@gmail.com>
2020-10-24 17:42:57 -0500

    added PG dep, and configured routing to DB

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+
+    let queryText = `UPDATE "galleryitems" SET "likes" = "likes" + 1 WHERE "id" = $1`
+    pool.query(queryText,[req.params.id]).then( (response) => {
+        console.log(response.rows, 'rows updated');
+        res.sendStatus(200)
+    }).catch( (error) => {
+        console.log('error', error);
+        res.sendStatus(500);
+    });
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    let queryText =  `SELECT * FROM "galleryitems" ORDER BY "id" DESC;`
+    pool.query(queryText).then( (results) =>{
+        console.log(results);
+        res.send(results.rows);
+    }).catch( (error) => {
+        console.log(error);
+    })
+   
 }); // END GET Route
 
 module.exports = router;
