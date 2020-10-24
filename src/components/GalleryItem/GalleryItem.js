6c2d72719b7c2eb7faf0ac92e8d5e696032a47e2
Khaildoran <sgermscheid@gmail.com>
2020-10-24 08:06:19 -0500

    conditional render working need better formatting

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -4,12 +4,33 @@ import './GalleryItem.css'
 
 class GalleryItem extends Component {
 
-  
+    state = {
+        renderImg: true
+    }
+
+    toggleRenderState = () => {
+        this.setState({
+            renderImg: !this.state.renderImg
+        });
+    }
+
+    renderitemBox = () => {
+        console.log('renderitembox');
+        
+        if (this.state.renderImg) {
+            console.log('in true render img state');
+            
+            return <img src={this.props.item.path} alt={this.props.item.name} className="itemBox" onClick={this.toggleRenderState}></img>
+        } else {
+            return <p className="itemBox" onClick={this.toggleRenderState}>test description text</p>
+        }
+    }
+
 
     render(){
         return (
             <div className="galItemFrame" key={this.props.item.id}>
-                <img src={this.props.item.path} alt={this.props.item.name}></img>
+                {this.renderitemBox()}
                     <button onClick={() => this.props.updateLike(this.props.item.id)}>likes</button> {/* accessing PUT request function in App, passing in item ID */}
                 <p>{this.props.item.likes}</p>
             </div>
